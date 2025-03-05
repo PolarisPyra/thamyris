a08b5e513e37071302a59f3a958e7ae23820fc6f
Taylor Miller-Klugman <tmillerklugman@gmail.com>
2025-03-05 15:53:26 -0500

    temp favicon

--- src/server.tsx
+++ src/server.tsx
@@ -92,6 +92,7 @@ const server = new Hono()
 				<head>
 					<meta charSet="utf-8" />
 					<meta name="viewport" content="width=device-width, initial-scale=1" />
+
 					<title>Thamyris</title>
 				</head>
 				<body>
