0889a0002af606e32ea201abbb7052e92ddfa126
PolarisPyra <tmillerklugman@gmail.com>
2025-03-12 14:11:50 -0400

    i love fixing on prod cuz i cant build in windows

--- src/pages/account/account.tsx
+++ src/pages/account/account.tsx
@@ -7,14 +7,7 @@ import { useAdminPermissions } from "@/hooks/admin/use-admin";
 
 const Account = () => {
 	const { isAdmin, isLoading: isCheckingAdmin } = useAdminPermissions();
-	const buildDate = (dateString: string) => {
-		const date = new Date(dateString);
-		const year = date.getFullYear();
-		const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
-		const day = String(date.getDate()).padStart(2, "0");
-		const hours = String(date.getHours()).padStart(2, "0");
-		return `${year}${month}${day}${hours}`;
-	};
+
 	return (
 		<div className="relative flex-1 overflow-auto">
 			<Header title={isAdmin ? "Admin Dashboard" : "Account Dashboard"} />
@@ -31,11 +24,11 @@ const Account = () => {
 							<div className="mx-auto max-w-2xl space-y-6">
 								<AimeCardSwap />
 							</div>
-							<div className="text-primary absolute bottom-0 left-0 p-2">Build Date: {buildDate(env.BUILD_DATE)}</div>
+							<div className="text-primary absolute bottom-0 left-0 p-2">Build Date: {env.BUILD_DATE}</div>
 						</>
 					)}
 				</div>
-				<div className="text-primary absolute bottom-0 left-0 p-2">Build Date: {buildDate(env.BUILD_DATE)}</div>
+				<div className="text-primary absolute bottom-0 left-0 p-2">Build Date: {env.BUILD_DATE}</div>
 			</div>
 		</div>
 	);
