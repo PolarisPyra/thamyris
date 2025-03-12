2fb2a288435a16382987ec6b00cc941d05194bff
PolarisPyra <tmillerklugman@gmail.com>
2025-03-12 14:18:30 -0400

    maybe

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,8 @@ const buildDate = (date) => {
 	const year = d.getFullYear();
 	const month = String(d.getMonth() + 1).padStart(2, "0");
 	const day = String(d.getDate()).padStart(2, "0");
-	const hours = String(d.getHours()).padStart(2, "0");
-	return `${year}${month}${day}${hours}`;
+	const time = String(d.getHours()).padStart(2, "0") + String(d.getMinutes()).padStart(2, "0");
+	return `${year}${month}${day}${time}`;
 };
 export default defineConfig({
 	ssr: {
