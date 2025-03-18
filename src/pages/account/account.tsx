cdab44384580ba4cef5f276ece859a8b520c9b23
PolarisPyra <tmillerklugman@gmail.com>
2025-03-18 06:29:53 -0400

    remove padding

--- src/pages/account/account.tsx
+++ src/pages/account/account.tsx
@@ -11,22 +11,20 @@ const Account = () => {
 	return (
 		<div className="relative flex-1 overflow-auto">
 			<Header title={isAdmin ? "Admin Dashboard" : "Account Dashboard"} />
-			<div className="p-6">
-				<div className="mx-auto max-w-2xl space-y-6">
-					{isCheckingAdmin ? (
-						<div className="rounded-lg bg-gray-800 p-6 shadow-md">
-							<p className="text-center">Checking permissions...</p>
+			<div className="mx-auto max-w-2xl space-y-6">
+				{isCheckingAdmin ? (
+					<div className="rounded-lg bg-gray-800 p-6 shadow-md">
+						<p className="text-center">Checking permissions...</p>
+					</div>
+				) : (
+					<>
+						{isAdmin && <KeychipGenerator />}
+						{!isAdmin}
+						<div className="mx-auto max-w-2xl space-y-6">
+							<AimeCardSwap />
 						</div>
-					) : (
-						<>
-							{isAdmin && <KeychipGenerator />}
-							{!isAdmin}
-							<div className="mx-auto max-w-2xl space-y-6">
-								<AimeCardSwap />
-							</div>
-						</>
-					)}
-				</div>
+					</>
+				)}
 			</div>
 		</div>
 	);
