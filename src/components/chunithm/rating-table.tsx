1c0bb48bd5778b68d6a43154d8c104dacb765ee7
Taylor Miller-Klugman <tmillerklugman@gmail.com>
2025-03-21 14:30:23 -0400

    added jacket path to rating table

--- src/components/chunithm/rating-table.tsx
+++ src/components/chunithm/rating-table.tsx
@@ -5,6 +5,7 @@ import { Search } from "lucide-react";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { ChunitmRating, getDifficultyFromChunithmChart } from "@/utils/helpers";
 import { getChunithmGrade } from "@/utils/helpers";
+import { cdnUrl } from "@/lib/constants";
 
 interface RatingTable {
 	id: number;
@@ -21,6 +22,7 @@ interface RatingTable {
 	level: number;
 	chartId: number;
 	genre: string;
+	jacketPath: string;
 }
 
 interface RatingFrameTableProps {
@@ -66,8 +68,18 @@ const ChunithmRatingFrameTable = ({ data, title }: RatingFrameTableProps) => {
 					<TableBody>
 						{filteredSongs.map((song, index) => (
 							<TableRow key={song.id ?? index} className="border-seperator hover:bg-hover border-b">
-								<TableCell className="text-primary max-w-[140px] truncate text-sm">{song.title}</TableCell>
-								<TableCell className="text-primary text-sm">{song.score?.toLocaleString()}</TableCell>
+							<TableCell className="text-primary text-sm">
+									<div className="flex items-center gap-3">
+										<img
+											width={50}
+											height={50}
+											src={`${cdnUrl}assets/jacket/${song.jacketPath.replace(".dds", ".png")}`}
+											alt={String(song.title)}
+											className="flex-shrink-0"
+										/>
+										<span className="text-primary truncate">{song.title}</span>
+									</div>
+								</TableCell>								<TableCell className="text-primary text-sm">{song.score?.toLocaleString()}</TableCell>
 								<TableCell className="text-primary text-sm">{getChunithmGrade(song.score!)}</TableCell>
 								<TableCell className="text-primary text-sm">{song.level}</TableCell>
 								<TableCell className="text-primary text-sm">
